52765f9cb909229c48919c7132f6f009216bb157
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2022-09-11 11:37:11 +0300

    add second task

---
+++ src/by/epam/trukhanovich/programming/simple_class02/MainSimpleClass2.java
@@ -0,0 +1,19 @@
+package by.epam.trukhanovich.programming.simple_class02;
+
+public class MainSimpleClass2 {
+
+    public static void main(String[] args) {
+
+        Test2 test1 = new Test2();
+        Test2 test2 = new Test2("Ночь", 1001);
+
+        test1.setNumber(2.0);
+        test1.setString("Java");
+
+        System.out.println("test1: " + test1.getNumber() + " " + test1.getString());
+        System.out.println("test2: " + test2.getNumber() + " " + test2.getString());
+
+    }
+}
+
+
---
+++ src/by/epam/trukhanovich/programming/simple_class02/Test2.java
@@ -0,0 +1,39 @@
+package by.epam.trukhanovich.programming.simple_class02;
+
+/*
+ (4). Programming with classes
+ Простейшие классы и объекты
+ 2. Создайте класс Test2 двумя переменными. Добавьте конструктор с входными параметрами. Добавьте конструктор,
+ инициализирующий члены класса по умолчанию. Добавьте set- и get- методы для полей экземпляра класса
+*/
+
+public class Test2 {
+
+    private String string;
+    private double number;
+
+    public Test2() {
+    }
+
+    public Test2(String string, double number) {
+        this.string = string;
+        this.number = number;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public void setNumber(double number) {
+        this.number = number;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public double getNumber() {
+        return number;
+    }
+
+}
